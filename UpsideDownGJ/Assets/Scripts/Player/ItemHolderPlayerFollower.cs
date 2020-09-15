f6096d29d24791ec4ec77697fcd5f5fdee404c90
milk9111 <connorlndbrg349@gmail.com>
2020-09-14 23:32:37 -0400

    Adding in the ability to highlight and equip (hold) items within range

---
+++ UpsideDownGJ/Assets/Scripts/Player/ItemHolderPlayerFollower.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHolderPlayerFollower : MonoBehaviour
+{
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var xPos = GetTrueXPosition();
+        var yPos = GetTrueYPosition();
+
+        //Debug.Log(xPos);
+        if (player.FlippedX())
+        {
+            //Debug.Log("flipped x");
+            xPos *= -1;
+        }
+        //Debug.Log(xPos);
+
+        if (player.FlippedY())
+        {
+            yPos *= -1;
+        }
+
+        transform.localPosition = new Vector2(xPos, yPos);
+    }
+
+    private float GetTrueXPosition()
+    {
+        return Mathf.Abs(transform.localPosition.x);
+    }
+
+    private float GetTrueYPosition()
+    {
+        return Mathf.Abs(transform.localPosition.y);
+    }
+}
---
+++ UpsideDownGJ/Assets/Scripts/Util.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public static class Util
+{
+    public static float CalculateDistance(Vector2 a, Vector2 b)
+    {
+        return Mathf.Abs(Vector2.Distance(a, b));
+    }
+}
