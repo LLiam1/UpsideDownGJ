128bef10dd5727c8c96413b3da933bf061eaa922
milk9111 <connorlndbrg349@gmail.com>
2020-09-18 17:50:59 -0400

    Adding box with inverse gravity

--- UpsideDownGJ/Assets/Scripts/IGravityObject.cs
+++ UpsideDownGJ/Assets/Scripts/IGravityObject.cs
@@ -4,4 +4,6 @@ public interface IGravityObject
     bool UsingGravity();
     void DisableGravity();
     void EnableGravity();
+    void SetGravityScale(float gravityScale);
+    float GetGravityScale();
 }
