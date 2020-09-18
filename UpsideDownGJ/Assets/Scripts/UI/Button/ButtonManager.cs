ad767a716536c776e8c7e23dbf6eaf664d0a21ae
LLiam1 <LEVEROCKLIaM@GMaIL.COM>
2020-09-18 16:41:19 -0400

    Begun Menu

---
+++ UpsideDownGJ/Assets/Scripts/UI/Button/ButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private bool isMouseHovering = false;
+    private Animator anim;
+
+    private void Start()
+    {
+        isMouseHovering = false;
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (isMouseHovering)
+        {
+            anim.StopPlayback();
+        } else
+        {
+            anim.Play(0);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isMouseHovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isMouseHovering = false;
+    }
+}
