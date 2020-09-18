fa66b3d16b8ac368785a108efb9dc0d1a5b06870
milk9111 <connorlndbrg349@gmail.com>
2020-09-17 22:34:17 -0400

    Adding gate open and close with button

--- UpsideDownGJ/Assets/Scripts/Activator/ActivatorController.cs
+++ UpsideDownGJ/Assets/Scripts/Activator/ActivatorController.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class ActivatorController : MonoBehaviour
 {
     public ActivatorConfiguration config;
+    public ButtonActivatedGate gate;
 
     private void Start()
     {
-        config.doorController = config.door.GetComponent<DoorController>();
+        if (config.door != null)
+        {
+            config.doorController = config.door.GetComponent<DoorController>();
+        }
     }
 
     private void Update()
     {
-        config.doorController.isOpened = config.isActivated;
+        if (config.doorController != null)
+        {
+            config.doorController.isOpened = config.isActivated;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         config.button.transform.position -= new Vector3(0, 0.15f, 0);
         config.isActivated = true;
+        if (gate != null)
+        {
+            gate.Activate();
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -29,8 +40,11 @@ public class ActivatorController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-
         config.button.transform.position += new Vector3(0, 0.15f, 0);
         config.isActivated = false;
+        if (gate != null)
+        {
+            gate.Activate();
+        }
     }
 }
