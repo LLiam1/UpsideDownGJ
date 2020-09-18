94f28e34b6263a3ea9c28858f6d5b71995507c3f
milk9111 <connorlndbrg349@gmail.com>
2020-09-18 13:40:07 -0400

    Adding load level on goal
    Also made some button gate updates

--- UpsideDownGJ/Assets/Scripts/GameController.cs
+++ UpsideDownGJ/Assets/Scripts/GameController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SceneFader))]
 public class GameController : MonoBehaviour
 {
+    public string nextLevelName;
     public bool switched;
 
+    public GameObject fadeScreen;
+
     private Rigidbody2D[] rigidBodies;
+    private SceneFader fader;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBodies = FindObjectsOfType<Rigidbody2D>();
         switched = false;
+        fader = GetComponent<SceneFader>();
+
+        fadeScreen.SetActive(true);
+        StartCoroutine(fader.Fade(SceneFader.FadeDirection.Out));
     }
 
     public void SwitchGravity()
@@ -38,5 +47,10 @@ public class GameController : MonoBehaviour
     public int GetGravityDirection()
     {
         return switched ? -1 : 1;
+    }
+
+    public void NextLevel()
+    {
+        StartCoroutine(fader.FadeAndLoadScene(SceneFader.FadeDirection.In, nextLevelName));
     }
 }
---
+++ UpsideDownGJ/Assets/Scripts/LevelGoal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour
+{
+    private GameController controller;
+
+    void Start()
+    {
+        controller = FindObjectOfType<GameController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            controller.NextLevel();
+        }
+    }
+}
