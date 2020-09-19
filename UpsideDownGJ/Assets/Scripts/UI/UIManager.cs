21279741f34a0d2895b67e6f37bed171edc2d97c
LLiam1 <LEVEROCKLIaM@GMaIL.COM>
2020-09-18 23:43:58 -0400

    UI Main Menu Update
    - Added Play Button Functionality.
    - Added Quit Button Functionality.

---
+++ UpsideDownGJ/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SceneFader))]
+public class UIManager : MonoBehaviour
+{
+    public string nextLevelName;
+
+    public GameObject fadeScreen;
+    private SceneFader fader;
+
+    void Start()
+    {
+        fader = GetComponent<SceneFader>();
+
+        fadeScreen.SetActive(false);
+        StartCoroutine(fader.Fade(SceneFader.FadeDirection.Out));
+    }
+    public void NextLevel()
+    {
+        StartCoroutine(fader.FadeAndLoadScene(SceneFader.FadeDirection.In, nextLevelName));
+    }
+
+    public void OpenLevelSelector()
+    {
+
+    }
+
+    public void DisplayCredits()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
